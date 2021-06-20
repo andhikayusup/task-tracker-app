e6274f6d10e54edefe9e5e91a4af9a43bd9ae908
andhikayusup <andeeka.ym@gmail.com>
2021-06-20 13:21:56 +0700

    Implement fetching tasks functionalities from mock server

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
+import { useState, useEffect } from 'react'
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
-import { useState } from 'react'
 import AddTask from "./components/AddTask";
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
 
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
